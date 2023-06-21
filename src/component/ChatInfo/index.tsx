44357cb31a8085510104c4107ea3c45cb5cd4ddc
Sashwat <sashwat@cataloging.ai>
2023-06-22 04:45:53 +0530

    add loader

--- src/component/ChatInfo/index.tsx
+++ src/component/ChatInfo/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { container } from 'tsyringe';
 import { IdentifyService } from '../../services/identify.service';
 
-import { Button, Card, Form, Input } from 'antd';
+import { Button, Card, Form, Input, Spin } from 'antd';
 
 
 const ChatInfo = () => {
 
     const identifyService = container.resolve(IdentifyService);
+    const [spinLoad, setSpinLoad] = React.useState(false)
     const [textResponse, setTextResponse] = React.useState({});
 
 
@@ -16,44 +17,50 @@ const ChatInfo = () => {
     );
 
     const onFinish = (values: any) => { // On submiting the form response
-        identifyService.createContact(values).then((r) => setTextResponse(r));
+        setSpinLoad(true)
+        identifyService.createContact(values).then((r) => {
+            setTextResponse(r);
+            setSpinLoad(false)
+        });
     }
-    return <div className='form-class'>
-        <Card title="User Info" style={{ width: 500, padding: 5 }}>
-            <Form
-                name="basic"
-                layout='vertical'
-                labelCol={{ span: 8 }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ type: 'email' }]}
+    return <Spin spinning={spinLoad} size="large">
+        <div className='form-class'>
+            <Card title="User Info" style={{ width: 500, padding: 5 }}>
+                <Form
+                    name="basic"
+                    layout='vertical'
+                    labelCol={{ span: 8 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    autoComplete="off"
                 >
-                    <Input />
-                </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ type: 'email' }]}
+                    >
+                        <Input />
+                    </Form.Item>
 
-                <Form.Item
-                    name="phoneNumber"
-                    label="Phone Number"
-                    rules={[{ message: 'Please input your phone number!' }]}
-                >
-                    <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-                </Form.Item>
+                    <Form.Item
+                        name="phoneNumber"
+                        label="Phone Number"
+                        rules={[{ message: 'Please input your phone number!' }]}
+                    >
+                        <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+                    </Form.Item>
 
-                <Form.Item >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-            <Input.TextArea autoSize={true} placeholder='Response after sumbit' value={Object.keys(textResponse).length !== 0 ? JSON.stringify(textResponse, null, 2): ""} />
-        </Card>
+                    <Form.Item >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <Input.TextArea autoSize={true} placeholder='Response after sumbit' value={Object.keys(textResponse).length !== 0 ? JSON.stringify(textResponse, null, 2) : ""} />
+            </Card>
 
-    </div>
+        </div>
+    </Spin>
 }
 
 export default ChatInfo;
